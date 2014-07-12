580353dc5e4eef0b9c6e5d59b45f93b607d2017b
Dmitry Leskov <git@dmitryleskov.com>
2014-07-12 21:08:33 +0700

    Rename Main to Part 1 Factor out StreamConsumers trait

--- src/streamhygiene/Part1.scala
+++ src/streamhygiene/Part1.scala
@@ -7,28 +7,7 @@ package streamhygiene
 import scala.annotation.tailrec
 import Test._
 
-trait StreamConsumers {
-  @tailrec
-  final def traitSumTailRec(xs: Stream[Int], z: Int = 0): Int = {
-    if (xs.isEmpty) z else traitSumTailRec(xs.tail, z + xs.head)
-  }
-  def traitSumImperative(xs: Stream[Int]): Int = {
-    var scan = xs
-    var res = 0
-    while (!scan.isEmpty) {
-      res += scan.head
-      scan = scan.tail
-    }
-    res
-  }
-  def traitSumByName(xs: => Stream[Int]): Int = {
-    @tailrec def loop(acc: Int, xs: Stream[Int]): Int =
-      if (xs.isEmpty) acc else loop(acc+xs.head, xs.tail)
-    loop(0, xs)
-  }
-}
-
-object Main extends StreamConsumers {
+object Part1 extends StreamConsumers {
   def ones: Stream[Int] = 1 #:: ones
    
   def sum(xs: Stream[Int]): Int = {
---
+++ src/streamhygiene/StreamConsumers.scala
@@ -0,0 +1,29 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package streamhygiene
+import scala.annotation.tailrec
+
+trait StreamConsumers {
+  @tailrec
+  final def traitSumTailRec(xs: Stream[Int], z: Int = 0): Int = {
+    if (xs.isEmpty) z else traitSumTailRec(xs.tail, z + xs.head)
+  }
+  def traitSumImperative(xs: Stream[Int]): Int = {
+    var scan = xs
+    var res = 0
+    while (!scan.isEmpty) {
+      res += scan.head
+      scan = scan.tail
+    }
+    res
+  }
+  def traitSumByName(xs: => Stream[Int]): Int = {
+    @tailrec def loop(acc: Int, xs: Stream[Int]): Int =
+      if (xs.isEmpty) acc else loop(acc+xs.head, xs.tail)
+    loop(0, xs)
+  }
+}
