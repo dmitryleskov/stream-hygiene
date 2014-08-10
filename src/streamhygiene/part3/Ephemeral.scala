15f8a924c8e39682b735cd594d40827796664855
Dmitry Leskov <git@dmitryleskov.com>
2014-08-10 17:27:35 +0700

    Cleanup sources for Part III

--- src/streamhygiene/part3/Ephemeral.scala
+++ src/streamhygiene/part3/Ephemeral.scala
@@ -43,7 +43,8 @@ object Ephemeral extends AutoConfig with EphemeralStreamConsumers {
   test("input.length"){
     input.length
   }     
- /* 
+
+  /* 
   *  No Rule #2: Functions consuming EphemeralStreams do not need to be tail-recursive.
   */
   test("Imperative sum(input)"){
@@ -100,29 +101,28 @@ object Ephemeral extends AutoConfig with EphemeralStreamConsumers {
     sumPatMat(input)
   }
   
-  test("average(input)"){
-    def average(xs: EphemeralStream[Int]): Option[Int] = {
+  test("tailAvg(input)"){
+    def tailAvg(xs: EphemeralStream[Int]): Option[Int] = {
       xs match {
         case y ##:: ys => Some(ys.sum / ys.length)
         case _ => None
       }
     }
-    average(input)
+    tailAvg(input)
   }  
 
 /*
- *  No Rule $5. All eager stream-consuming methods work (if the stream is finite).
+ *  No Rule #5. All eager stream-consuming methods work (if the stream is finite).
  *              Not all Stream methods are available, however, and 
  *              EphemeralStream's own methods need special treatment.
  */  
-  def plus(x: => Int)(y: => Int) = x+y
-  test("input.foldLeft(0)(plus)"){input.foldLeft(0)(plus)}     
+  test("input.foldLeft(0)(x => y => x + y)"){input.foldLeft(0)(x => y => x + y)}     
   test("input.reduceLeft(_ + _)"){input.reduceLeft(_ + _)}     
   test("{var sum = 0; input.foreach(x => sum += x); sum}"){
     var sum = 0; input.foreach(x => sum += x); sum
   }     
   test("input.sum"){input.sum}
-  test("(0 /: (input))(_ + _)"){(0 /: (input))(_ + _)}
+  test("(0 /: input)(_ + _)"){(0 /: input)(_ + _)}
   test("input forall (_ == 1)"){input forall (_ == 1)}
   test("input exists (_ != 1)"){input exists (_ != 1)}
   test("input find (_ != 1)"){input find (_ != 1)}
